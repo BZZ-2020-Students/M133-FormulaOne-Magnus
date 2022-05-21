81f89502613abb64619c7a3f935c8b4a87c88c81
Mag <magnusgoetz0@gmail.com>
2022-05-21 11:45:42 +0200

    project

---
+++ formulaOne/formulaOne/src/main/java/ch/formula/one/formulaone/HelloApplication.java
@@ -0,0 +1,9 @@
+package ch.formula.one.formulaone;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
